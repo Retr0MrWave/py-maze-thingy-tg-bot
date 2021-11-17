3ee7b4a7d1b7ad8d52e5178c5b746482c026e7fe
Ilia Zhuravok <ilia.zhuravok@gmail.com>
2021-11-18 01:58:18 +0200

    Add movement

--- MazeHandling.py
+++ MazeHandling.py
@@ -5,23 +5,38 @@ class Maze:
     def __init__(self, width, height):
         self.maze_arr = mg.generateMaze(width, height)
 
-        potential_player = (1, 1)
+        potential_player = [1, 1]
         while (self.maze_arr[potential_player[0]][potential_player[1]] != 'c'):
-            potential_player = (random.randint(1, width//2), random.randint(1, height//2))
+            potential_player = [random.randint(1, width//2), random.randint(1, height//2)]
         self.player = potential_player
         
-        potential_exit = (width-1, height-1)
+        potential_exit = [width-1, height-1]
         while (self.maze_arr[potential_exit[0]][potential_exit[1]] != 'c'):
-            potential_exit = (random.randint(width//2 + 1, width-1), random.randint(height//2 + 1, height-1))
+            potential_exit = [random.randint(width//2 + 1, width-1), random.randint(height//2 + 1, height-1)]
         self.exit = potential_exit
+    
+    def movePlayer(self, direction):
+        x, y = self.player[0], self.player[1]
+        if direction == 'w':
+            if self.maze_arr[x-1][y] != 'w':
+                self.player[0] -= 1
+        elif direction == 's':
+            if self.maze_arr[x+1][y] != 'w':
+                self.player[0] += 1
+        elif direction == 'a':
+            if self.maze_arr[x][y-1] != 'w':
+                self.player[1] -= 1
+        elif direction == 'd':
+            if self.maze_arr[x][y+1] != 'w':
+                self.player[1] += 1
     
     def mazeToString(self):
         res = ""
         for i in range(0, len(self.maze_arr)):
             for j in range(0, len(self.maze_arr[0])):
-                if (i, j) == self.player:
+                if [i, j] == self.player:
                     res += "@ "
-                elif (i, j) == self.exit:
+                elif [i, j] == self.exit:
                     res += "E "
                 elif self.maze_arr[i][j] == 'w':
                     res += "▮ "
--- main.py
+++ main.py
@@ -2,7 +2,10 @@ import MazeHandling as mh
 
 def createMaze():
     print("Please enter the width and height of your desired maze:")
-    width, height = map(int, input().split())
+    s = input()
+    if s == 'q':
+        exit()
+    width, height = map(int, s.split())
     if width < 5 or height < 5:
         print("We\'re sorry, but our generation algorithm doesn't work for mazes smaller than 5 in each dimention.")
         exit()
@@ -24,5 +27,10 @@ if __name__ == "__main__":
             printHelpMessage()
         elif (command == 'r'):
             current_maze = createMaze()
+        elif (command == 'w' or command == 'a' or command == 's' or command == 'd'):
+            current_maze.movePlayer(command)
         else:
             print("Unrecognised command. Type h for help.")
+        if current_maze.player == current_maze.exit:
+            print("Congratulations! You solved the maze")
+            current_maze = createMaze()
