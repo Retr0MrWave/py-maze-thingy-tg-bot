c394e4fff061938c393ac9454bbb81eddb2a356a
Ilia Zhuravok <ilia.zhuravok@gmail.com>
2021-11-19 11:25:24 +0200

    Made into a telegram bot

--- main.py
+++ main.py
@@ -1,36 +1,98 @@
 import MazeHandling as mh
 
-def createMaze():
-    print("Please enter the width and height of your desired maze:")
-    s = input()
-    if s == 'q':
-        exit()
-    width, height = map(int, s.split())
-    if width < 5 or height < 5:
-        print("We\'re sorry, but our generation algorithm doesn't work for mazes smaller than 5 in each dimention.")
-        exit()
-    return mh.Maze(width, height)
+games_dict = {"":""}
 
-def printHelpMessage():
-    # TODO: Make a proper help message
-    print("We\'ll make this later")
+import logging
 
-if __name__ == "__main__":
-    print("Welcome to maze thingy!")
-    current_maze = createMaze()
-    while True:
-        print(current_maze.mazeToString())
-        command = input("Your input: (type h for help)\n")
-        if (command == 'q'):
-            exit()
-        elif (command == 'h'):
-            printHelpMessage()
-        elif (command == 'r'):
-            current_maze = createMaze()
-        elif (command == 'w' or command == 'a' or command == 's' or command == 'd'):
-            current_maze.movePlayer(command)
-        else:
-            print("Unrecognised command. Type h for help.")
-        if current_maze.player == current_maze.exit:
-            print("Congratulations! You solved the maze")
-            current_maze = createMaze()
+from telegram import Update
+from telegram.ext import Updater, CommandHandler, CallbackContext
+
+# Enable logging
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
+)
+
+logger = logging.getLogger(__name__)
+
+
+# Define a few command handlers. These usually take the two arguments update and
+# context. Error handlers also receive the raised TelegramError object in error.
+# Best practice would be to replace context with an underscore,
+# since context is an unused local variable.
+# This being an example and not having context present confusing beginners,
+# we decided to have it present as context.
+def start(update: Update, context: CallbackContext) -> None:
+    """Sends explanation on how to use the bot."""
+    games_dict[update.message.chat_id] = mh.Maze(15, 15)
+    update.message.reply_text("```\n" + games_dict[update.message.chat_id].mazeToString() + "\n```", parse_mode="MarkdownV2")
+        
+def go_up(update: Update, context: CallbackContext) -> None:
+    """Add a job to the queue."""
+    chat_id = update.message.chat_id
+    games_dict[chat_id].movePlayer("w")
+    if (games_dict[chat_id].player == games_dict[chat_id].exit):
+        update.message.reply_text("You win\. Type `/start` to start again", parse_mode = "MarkdownV2")
+        games_dict.pop(chat_id)
+        return
+    update.message.reply_text("```\n" + games_dict[update.message.chat_id].mazeToString() + "\n```", parse_mode="MarkdownV2")
+
+def go_left(update: Update, context: CallbackContext) -> None:
+    """Add a job to the queue."""
+    chat_id = update.message.chat_id
+    games_dict[chat_id].movePlayer("a")
+    if (games_dict[chat_id].player == games_dict[chat_id].exit):
+        update.message.reply_text("You win\. Type `/start` to start again", parse_mode = "MarkdownV2")
+        games_dict.pop(chat_id)
+        return
+    update.message.reply_text("```\n" + games_dict[update.message.chat_id].mazeToString() + "\n```", parse_mode="MarkdownV2")
+
+def go_down(update: Update, context: CallbackContext) -> None:
+    """Add a job to the queue."""
+    chat_id = update.message.chat_id
+    games_dict[chat_id].movePlayer("s")
+    if (games_dict[chat_id].player == games_dict[chat_id].exit):
+        update.message.reply_text("You win\. Type `/start` to start again", parse_mode = "MarkdownV2")
+        games_dict.pop(chat_id)
+        return
+    update.message.reply_text("```\n" + games_dict[update.message.chat_id].mazeToString() + "\n```", parse_mode="MarkdownV2")
+
+def go_right(update: Update, context: CallbackContext) -> None:
+    """Add a job to the queue."""
+    chat_id = update.message.chat_id
+    games_dict[chat_id].movePlayer("d")
+    if (games_dict[chat_id].player == games_dict[chat_id].exit):
+        update.message.reply_text("You win\. Type `/start` to start again", parse_mode = "MarkdownV2")
+        games_dict.pop(chat_id)
+        return
+    update.message.reply_text("```\n" + games_dict[update.message.chat_id].mazeToString() + "\n```", parse_mode="MarkdownV2")
+
+def main() -> None:
+    """Run bot."""
+    # Create the Updater and pass it your bot's token.
+    tokenfile = open("token.txt", 'r')
+    token = tokenfile.readline()
+    updater = Updater(token)
+    tokenfile.close()
+
+    # Get the dispatcher to register handlers
+    dispatcher = updater.dispatcher
+
+    # on different commands - answer in Telegram
+    dispatcher.add_handler(CommandHandler("start", start))
+    dispatcher.add_handler(CommandHandler("help", start))
+    dispatcher.add_handler(CommandHandler("w", go_up))
+    dispatcher.add_handler(CommandHandler("a", go_left))
+    dispatcher.add_handler(CommandHandler("s", go_down))
+    dispatcher.add_handler(CommandHandler("d", go_right))
+
+    # Start the Bot
+    updater.start_polling()
+
+    # Block until you press Ctrl-C or the process receives SIGINT, SIGTERM or
+    # SIGABRT. This should be used most of the time, since start_polling() is
+    # non-blocking and will stop the bot gracefully.
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
